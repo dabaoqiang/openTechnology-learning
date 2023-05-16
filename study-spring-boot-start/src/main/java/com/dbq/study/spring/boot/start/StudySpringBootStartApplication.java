8d90042e8872426b775d9a4cc44830e11ddf7134
大宝强 <1307536605@qq.com>
2023-05-16 22:20:16 +0800

    [feat] 初始化Spring Boot项目

---
+++ study-spring-boot-start/src/main/java/com/dbq/study/spring/boot/start/StudySpringBootStartApplication.java
@@ -0,0 +1,16 @@
+package com.dbq.study.spring.boot.start;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author dabaoqiang
+ */
+@SpringBootApplication
+public class StudySpringBootStartApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudySpringBootStartApplication.class, args);
+	}
+
+}
---
+++ study-spring-boot-start/src/test/java/com/dbq/study/spring/boot/start/StudySpringBootStartApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dbq.study.spring.boot.start;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudySpringBootStartApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
