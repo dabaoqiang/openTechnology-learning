cfba4f3846a7fe3bae30cdb63cde3f8f99ac414f
大宝强 <1307536605@qq.com>
2023-05-17 23:20:35 +0800

    [feat] 新增异常处理。
    待优化。

---
+++ study-spring-boot-exception/src/main/java/com/dbq/study/spring/boot/exception/StudySpringBootExceptionApplication.java
@@ -0,0 +1,16 @@
+package com.dbq.study.spring.boot.exception;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author dabaoqiang
+ */
+@SpringBootApplication
+public class StudySpringBootExceptionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudySpringBootExceptionApplication.class, args);
+	}
+
+}
---
+++ study-spring-boot-exception/src/test/java/com/dbq/study/spring/boot/exception/StudySpringBootExceptionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dbq.study.spring.boot.exception;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudySpringBootExceptionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
